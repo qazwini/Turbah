0ec2421329024d26b515555645d9bd5106f6e783
qazwini <mmqazwini@yahoo.com>
2020-07-15 18:30:40 -0400

    Added incompleted Turbah. Added place button with not functionality

--- Turbah/AppDelegate.swift
+++ Turbah/AppDelegate.swift
@@ -13,9 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupScreen()
         return true
     }
 
@@ -34,6 +34,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    
+    // MARK: - Custom Funcs
+    
+    func setupScreen() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        let navVC = UINavigationController(rootViewController: ViewController())
+        navVC.setNavigationBarHidden(true, animated: false)
+        
+        window?.rootViewController = navVC
     }
 
 
--- Turbah/ViewController.swift
+++ Turbah/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    var arView = ARView()
+    var placeButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        arView.addCoaching()
+        
+        let anchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.2, 0.2])
+        arView.scene.addAnchor(anchor)
+        
+        let boxAnchor = try! Turbah.loadScene()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+    }
+    
+    func setupUI() {
+        view = arView
+        
+        placeButton.translatesAutoresizingMaskIntoConstraints = false
+        placeButton.backgroundColor = .white
+        placeButton.layer.cornerRadius = 40
+        placeButton.addTarget(self, action: #selector(placeTurbah), for: .touchUpInside)
+        view.addSubview(placeButton)
+        NSLayoutConstraint.activate([
+            placeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            placeButton.heightAnchor.constraint(equalToConstant: 80),
+            placeButton.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+    
+    @objc func placeTurbah() {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+}
+
+extension ARView: ARCoachingOverlayViewDelegate {
+    func addCoaching() {
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.delegate = self
+        coachingOverlay.session = self.session
+        coachingOverlay.goal = .horizontalPlane
+        coachingOverlay.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(coachingOverlay)
+        NSLayoutConstraint.activate([
+            coachingOverlay.topAnchor.constraint(equalTo: topAnchor),
+            coachingOverlay.trailingAnchor.constraint(equalTo: trailingAnchor),
+            coachingOverlay.bottomAnchor.constraint(equalTo: bottomAnchor),
+            coachingOverlay.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+    }
+    
+    public func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        //Ready to add entities next?
     }
 }
