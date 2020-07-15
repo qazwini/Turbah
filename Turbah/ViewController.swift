bd6a252ae0c9c12628dee8003cbde10f5ed0eab7
qazwini <mmqazwini@yahoo.com>
2020-07-15 16:56:18 -0400

    initial commit

---
+++ Turbah/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turbah
+//
+//  Created by MMQ on 7/15/20.
+//  Copyright © 2020 MMQ. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
