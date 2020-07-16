32e98d103c8aedd1d3fca82e7f8a472b66ed7adf
qazwini <mmqazwini@yahoo.com>
2020-07-16 02:38:48 -0400

    added several more holy places coodinates

--- Turbah/Controllers/ViewController.swift
+++ Turbah/Controllers/ViewController.swift
@@ -148,9 +148,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ARCoachingOve
         let lon: Double
         
         static let kabah = coordinates(lat: 21.422487, lon: 39.826206)
-        
-        static let ali = coordinates(lat: 32.6164, lon: 44.0324)
+        static let aqsa = coordinates(lat: 31.7761, lon: 35.2358)
+        static let prophet = coordinates(lat: 24.4707, lon: 39.6307)
+        static let ali = coordinates(lat: 31.9957, lon: 44.3148)
         static let hussain = coordinates(lat: 32.6164, lon: 44.0324)
+        static let reza = coordinates(lat: 36.2878, lon: 59.6155)
+        static let askariain = coordinates(lat: 34.19878, lon: 43.87338)
+        static let baqi = coordinates(lat: 24.4672, lon: 39.6138)
+        static let masuma = coordinates(lat: 34.6418, lon: 50.8790)
+        static let zainab = coordinates(lat: 33.4444, lon: 36.3409)
     }
     
     var location: CLLocation?
