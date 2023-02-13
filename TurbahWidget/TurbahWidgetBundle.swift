482cfd6391615b6e8648987377095c680a28df22
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-02-13 15:09:08 +0600

    Create TurbahWidgetBundle.swift

---
+++ TurbahWidget/TurbahWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  TurbahWidgetBundle.swift
+//  TurbahWidget
+//
+//  Created by Ongar.dev on 13/02/2023.
+//  Copyright © 2023 MMQ. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TurbahWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TurbahWidget()
+    }
+}
